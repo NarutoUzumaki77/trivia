c421d9ea69820759956c0246f157132f0188612b
Gilbert Nwankwo <gilbertnwankwo@Gilberts-MacBook-Pro.local>
2019-11-08 18:55:40 -0500

    setup db and create tables

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,10 +1,9 @@
 import os
 from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
 
-from ..models import setup_db, Question, Category
+from models import *
 
 QUESTIONS_PER_PAGE = 10
 
@@ -13,11 +12,16 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
-    CORS(app)
 
     # TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     # TODO: Use the after_request decorator to set Access-Control-Allow
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
     # TODO: Create an endpoint to handle GET requests for all available categories.
 
