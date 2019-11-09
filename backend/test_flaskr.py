9ed7c09cb7b15a23dc23d1748190a85485617f81
Gilbert Nwankwo <gilbertnwankwo@Gilberts-MacBook-Pro.local>
2019-11-08 21:25:36 -0500

    complete setup of unittest

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -11,11 +11,10 @@ class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
     def setUp(self):
-        """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}@{}/{}".format('gilbertnwankwo', 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,15 +23,27 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
-    def tearDown(self):
-        """Executed after reach test"""
-        pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+            category1 = Category(type='IT')
+            category2 = Category(type='Sport')
+            self.db.session.add_all([category1, category2])
+            self.db.session.commit()
+
+    def tearDown(self):
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            categories = self.db.session.query(Category).all()
+            for category in categories:
+                self.db.session.delete(category)
+                self.db.session.commit()
+
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
 
 
 # Make the tests conveniently executable
